5f2445776c7073169791876ae94bd421fa08f1f6
evsikov145 <hikarokayto5@gmail.com>
2019-12-03 00:35:37 +0300

    Упрощение до одной функции

--- index.js
+++ index.js
@@ -9,13 +9,12 @@ const goods = [
     {title: 'Glasses', price: 850}
 ];
 
-const renderGoodsItem = (title = "наименование товара отсутствует", price = "цена товара отсутствует") => {
+/*const renderGoodsItem = (title = "наименование товара отсутствует", price = "цена товара отсутствует") => {
     return `<div class="list-item"><img src="foto.jpg"><h3>${title}</h3><p>${price}</p></div>`
-};
+};*/
 
 const renderGoodsList = (list = "список товаров отсутствует") => {
-    const goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-    
+    const goodsList = list.map(item => {return `<div class="list-item"><img src="foto.jpg"><h3>${item.title}</h3><p>${item.price}</p></div>`});
     document.querySelector('.list').innerHTML = goodsList.join("");
 };
 
