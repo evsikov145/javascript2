304127fdcc1816e37f96f1bc5667f029fec5caca
evsikov145 <hikarokayto5@gmail.com>
2019-12-03 00:32:27 +0300

    Стилизация

---
+++ index.js
@@ -0,0 +1,22 @@
+const goods = [
+    {title: 'Shirt', price: 150},
+    {title: 'Jacket', price: 250},
+    {title: 'Shoes', price: 350},
+    {title: 'Hat', price: 450},
+    {title: 'Coat', price: 550},
+    {title: 'T-shirt', price: 650},
+    {title: 'Shorts', price: 750},
+    {title: 'Glasses', price: 850}
+];
+
+const renderGoodsItem = (title = "наименование товара отсутствует", price = "цена товара отсутствует") => {
+    return `<div class="list-item"><img src="foto.jpg"><h3>${title}</h3><p>${price}</p></div>`
+};
+
+const renderGoodsList = (list = "список товаров отсутствует") => {
+    const goodsList = list.map(item => renderGoodsItem(item.title, item.price));
+    
+    document.querySelector('.list').innerHTML = goodsList.join("");
+};
+
+renderGoodsList(goods);
