e86af8fc8aa5205d966c81055e05f5c4c40fc59a
evsikov145 <hikarokayto5@gmail.com>
2019-12-23 00:00:01 +0300

    Начал выполнение

---
+++ main.js
@@ -0,0 +1,50 @@
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    goods: [],
+    filteredGoods: [],
+    searchLine: ''
+  },
+  methods: {
+    makeGETRequest(url) {
+      return new Promise((resolve, reject) => {
+        let xhr;
+        if (window.XMLHttpRequest) {
+          xhr = new window.XMLHttpRequest();
+        } else {
+          xhr = new window.ActiveXObject('Microsoft.XMLHTTP');
+        }
+
+        xhr.onreadystatechange = function () {
+          if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+              const body = JSON.parse(xhr.responseText);
+              resolve(body)
+            } else {
+              reject(xhr.responseText);
+            }
+          }
+        };
+        xhr.onerror = function (err) {
+          reject(err);
+        };
+
+        xhr.open('GET', url);
+        xhr.send();
+      });
+    }
+  },
+  async mounted() {
+    try {
+      this.goods = await this.makeGETRequest(`${API_URL}/catalogData.json`);
+      this.filteredGoods = [...this.goods];
+    } catch (e) {
+      console.error(e);
+    }
+  },
+  filterGoods() {
+
+  }
+});
