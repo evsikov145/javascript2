f80f9217ac0f1d1b9ab9bc4cd6263100461a8a09
evsikov145 <hikarokayto5@gmail.com>
2020-01-06 16:22:11 +0300

    Добавление поиска как компонента

--- main.js
+++ main.js
@@ -1,26 +1,6 @@
 const cart = [];
 
-Vue.component('search', {
-  props: {
-    searchLine: {
-      type: String,
-      required: false,
-      default: '',
-    }
-  },
-  template: `
-      <form class="search-form" @submit.prevent>
-        <input type="text" class="search-input" :value="searchLine"
-          @input="updateSearchLine"/>
-      </form>
-  `,
-  methods: {
-    updateSearchLine(val) {
-      const value = val.target.value;
-      this.$emit('update:searchLine', value);
-    }
-  }
-});
+import {search} from './components/search.js';
 
 Vue.component('cart', {
   data: () => ({
